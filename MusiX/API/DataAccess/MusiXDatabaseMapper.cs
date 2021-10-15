3e149b4092169ca33e4a48dd9e98b13b6e910f18
ruben <462404@student.fontys.nl>
2021-10-15 23:09:08 +0200

    Setup DatabaseMapper

---
+++ MusiX/API/DataAccess/MusiXDatabaseMapper.cs
@@ -0,0 +1,17 @@
+﻿using API.Models;
+using FluentNHibernate.Mapping;
+
+namespace API.DataAccess
+{
+    public class UserModelMapper : ClassMap<UserModel>
+    {
+        public UserModelMapper()
+        {
+            Table("user");
+            Id(i => i.Id);
+            Map(i => i.CreationDate);
+            Map(i => i.UserName);
+            Map(i => i.Email);
+        }
+    }
+}
