9a73d776a79ec62e0798325132e82feb806f1f26
ruben <462404@student.fontys.nl>
2021-10-15 23:07:48 +0200

    Setup Microsoft Identity

--- MusiX/API/Startup.cs
+++ MusiX/API/Startup.cs
@@ -1,16 +1,16 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.DataAccess;
+using API.Services;
+using API.DataAccess.Repositories;
 
 namespace API
 {
@@ -22,10 +22,43 @@ namespace API
         }
 
         public IConfiguration Configuration { get; }
+        public string IdentityDatabaseConnectionString { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Connection strings for the Identity en MusiX database
+#if DEBUG
+            IdentityDatabaseConnectionString = Configuration.GetConnectionString("LocalIdentityDatabaseConnection");
+#else
+            IdentityDatabaseConnectionString = Configuration.GetConnectionString("LiveIdentityDatabaseConnection");
+#endif
+
+            /// Allow cors
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                                  .AllowAnyMethod()
+                                  .AllowAnyHeader());
+            });
+
+            /// Database connection for identity
+            services.AddDbContext<IdentityDatabaseContext>(options => options.UseMySql(IdentityDatabaseConnectionString, 
+                ServerVersion.AutoDetect(IdentityDatabaseConnectionString)));
+
+            /// Default identity user
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                /// Paswword must be atleast 8 characters long
+                /// must contain a digit, uppercase, lowercase and 1 special character
+                options.Password.RequiredLength = 8;
+                options.Password.RequireDigit = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredUniqueChars = 1;
+                options.Password.RequireLowercase = true;
+            }).AddEntityFrameworkStores<IdentityDatabaseContext>()
+               .AddDefaultTokenProviders();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -37,6 +70,8 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("CorsPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -47,6 +82,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
